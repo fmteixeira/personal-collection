e3039cfe27e217e1d63efb38ec66c50871b4e870
fmteixeira <black_trim@hotmail.com>
2021-08-07 03:14:26 +0000

    added eslint react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "airbnb-typescript",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
   overrides: [
