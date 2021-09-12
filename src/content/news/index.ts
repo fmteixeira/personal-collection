a0768aa5bdc68f6c8ddc348ba458f86a2789551e
fmteixeira <black_trim@hotmail.com>
2021-09-12 13:59:49 +0000

    fixed error

--- src/content/news/index.ts
+++
@@ -1,8 +0,0 @@
-import inauguracao from "./inauguracao.json";
-import primeiroProjeto from "./primeiroProjeto.json";
-
-import { NewsArticle } from "@/types/interfaces";
-
-const news: NewsArticle[] = [inauguracao, primeiroProjeto];
-
-export default news;
